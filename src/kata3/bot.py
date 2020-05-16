6785c21370cceefd5d44815a161ec398db0cf374
vkamg <veronica.montoro.gonzalez@gmail.com>
2020-05-16 20:02:00 +0200

    kata 3 terminada - en mi telegram el bot funciona bien pero en el test todo da errar, no consigo averiguar por qué pasa eso

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,25 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    update.message.reply_text('Hola, Geeks!')
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    update.message.reply_text('Ayudame!')
 
 def mayus(update, context):
         #
-        return ""
+    palabra = str(context.args[0]).upper()
+    update.message.reply_text(palabra)
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
     #
-    return ""
+    palabra = str(update.message.text)
+    resultado = palabra[::-1]
+    if update.message.text:
+        update.message.reply_text(resultado)
+    
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -34,19 +39,24 @@ def error(update, context):
 def main():
     """Inicio del Bot"""
     #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    updater = Updater("1118119446:AAEc55HOynl1ltJfzPHs5vaQy1hElTmHCNM", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher
+    #
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
     #
     #
     #
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("mayus", mayus))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
     #
-    
+    dp.add_handler(MessageHandler(Filters.text, alreves))
+
     # Y este espera al error
     dp.add_error_handler(error)
 
